7de883e1a936ff11f6e77fe8efe0f5e865eec10b
Lucas Moura <lucas.moura@inteliger.com.br>
2022-08-13 12:38:43 -0300

    Adicionado arquivo para as funcoes de ajax Adicionado validacao de login para as configuracoes do financeiro

--- finance/urls.py
+++ finance/urls.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from django.views.generic import RedirectView
 
 from finance import views
+from finance import ajax
 
 urlpatterns = [
     # path('dashboard/', include(('finance.financeiro_dashboard.urls', 'finance.financeiro_dashboard'), namespace='dashboard')),
@@ -10,17 +11,17 @@ urlpatterns = [
     re_path('^configracoes$', views.Configuracoes.as_view(), name='configuracoes'),
 
     # Ajax functions
-    re_path(r'^account', views.BankAccount().as_view(), name='bank_account'),
-    re_path(r'^card$', views.CreditCard().as_view(), name='credit_card'),
-    re_path(r'^category$', views.Category().as_view(), name='category'),
-    re_path(r'^fatura$', views.Bill().as_view(), name='fatura'),
+    re_path(r'^account$', ajax.BankAccount().as_view(), name='bank_account'),
+    re_path(r'^card$', ajax.CreditCard().as_view(), name='credit_card'),
+    re_path(r'^category$', ajax.Category().as_view(), name='category'),
+    re_path(r'^fatura$', ajax.Bill().as_view(), name='fatura'),
 
-    re_path(r'^extrato$', views.Statement().as_view(), name='statement'),
-    re_path(r'^ajax/investmento$', views.Investment.as_view(), name='investment'),
-    re_path(r'^ajax/fatura/csv', views.Csv.as_view(), name='csv_fatura'),
-    re_path(r'^ajax/periodos', views.Periodos.as_view(), name='periods'),
-    re_path(r'^ajax/currency', views.Currency.as_view(), name='currency'),
-    re_path(r'^ajax/payment/date', views.PaymentDate.as_view(), name='payment_date'),
-    re_path(r'^ajax/expenses/fixed', views.FixedExpenses.as_view(), name='fixed_expenses'),
-    re_path(r'^ajax/expenses/evolution', views.ExpensesEvolution.as_view(), name='evolution_expenses'),
+    re_path(r'^extrato$', ajax.Statement().as_view(), name='statement'),
+    re_path(r'^ajax/investmento$', ajax.Investment.as_view(), name='investment'),
+    re_path(r'^ajax/fatura/csv$', ajax.Csv.as_view(), name='csv_fatura'),
+    re_path(r'^ajax/periodos$', ajax.Periodos.as_view(), name='periods'),
+    re_path(r'^ajax/currency$', ajax.Currency.as_view(), name='currency'),
+    re_path(r'^ajax/payment/date$', ajax.PaymentDate.as_view(), name='payment_date'),
+    re_path(r'^ajax/expenses/fixed$', ajax.FixedExpenses.as_view(), name='fixed_expenses'),
+    re_path(r'^ajax/expenses/evolution$', ajax.ExpensesEvolution.as_view(), name='evolution_expenses'),
 ]
