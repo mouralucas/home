c8af4951262fa8b9d30710a1e852e0e424141152
Lucas Moura <lucas.moura@inteliger.com>
2022-09-07 14:10:00 -0300

    Ajustado local das views de core Ajustado path das urls de core Retirada de arquivos e codigos nao utilizados

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,14 @@
 from django.urls import re_path, include
 
-import core.ajax
+import core.views
 
 urlpatterns = [
     re_path(r'^temp/', include('core.temp.urls')),
 
-    re_path(r'^ajax/module$', core.ajax.Module.as_view(), name='module'),
-    re_path(r'^ajax/category$', core.ajax.Category.as_view(), name='category'),
+    re_path(r'^module$', core.views.Module.as_view(), name='module'),
+    re_path(r'^category$', core.views.Category.as_view(), name='category'),
 
     # Country related views
-    re_path(r'^ajax/country$', core.ajax.Country.as_view(), name='country'),
-    re_path(r'^ajax/country/states', core.ajax.State.as_view(), name='country_state'),
+    re_path(r'^country$', core.views.Country.as_view(), name='country'),
+    re_path(r'^country/states', core.views.State.as_view(), name='country_state'),
 ]
