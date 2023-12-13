07ef13a77e94c4687cbe8d4cbf1cd88fe38f6592
Lucas Moura <lucaspenha471@gmail.com>
2023-12-13 11:37:35 -0300

    Add variables to periodicity in bcb integration

--- service/integration/bcb.py
+++ service/integration/bcb.py
@@ -3,6 +3,10 @@ import pandas as pd
 import finance.models
 from service.integration.integration import Integration
 
+DAILY_PERIODICITY = 'b9f83ad5-7701-4098-bdaf-ee092f3247eb'
+MONTHLY_PERIODICITY = 'dc5b3bf8-2b84-423a-9a90-e7e194e355fa'
+YEARLY_PERIODICITY = '9f4980b8-9c8f-4cce-90e2-af3c03867059'
+
 
 class BancoCentralAPI(Integration):
     """
@@ -33,9 +37,9 @@ class BancoCentralAPI(Integration):
         cdi_interest_daily = pd.read_json(self.url_bcdata.format('12', ''))
         cdi_interest_daily['data'] = pd.to_datetime(cdi_interest_daily['data'], dayfirst=True)
 
-        historical_cdi_ad_data = finance.models.FinanceData.objects.filter(index_id='2a2b100f-17d9-4c61-b3b4-f06662113953',
-                                                                           periodicity='b9f83ad5-7701-4098-bdaf-ee092f3247eb')
-        historical_cdi_ad_data.delete()
+        historical_cdi_daily_data = finance.models.FinanceData.objects.filter(index_id='2a2b100f-17d9-4c61-b3b4-f06662113953',
+                                                                              periodicity=DAILY_PERIODICITY)
+        historical_cdi_daily_data.delete()
         list_cdi_historical = []
         for idx, item in cdi_interest_daily.iterrows():
             aux = finance.models.FinanceData(
@@ -55,7 +59,7 @@ class BancoCentralAPI(Integration):
         cdi_interest_monthly['data'] = pd.to_datetime(cdi_interest_monthly['data'], dayfirst=True)
 
         historical_cdi_monthly_data = finance.models.FinanceData.objects.filter(index_id='2a2b100f-17d9-4c61-b3b4-f06662113953',
-                                                                                periodicity='dc5b3bf8-2b84-423a-9a90-e7e194e355fa')
+                                                                                periodicity=MONTHLY_PERIODICITY)
         historical_cdi_monthly_data.delete()
         list_cdi_historical_monthly = []
         for idx, item in cdi_interest_monthly.iterrows():
@@ -81,15 +85,15 @@ class BancoCentralAPI(Integration):
         print(cdi_interest_daily)
 
     def historical_data_selic(self):
-        selic_interest_ad = pd.read_json(self.url_bcdata.format('11', ''))
-        selic_interest_ad['data'] = pd.to_datetime(selic_interest_ad['data'], dayfirst=True)
+        selic_interest_daily = pd.read_json(self.url_bcdata.format('11', ''))
+        selic_interest_daily['data'] = pd.to_datetime(selic_interest_daily['data'], dayfirst=True)
 
-        historical_selic_ad_data = finance.models.FinanceData.objects.filter(index_id='b7e5c4a0-3b65-4b1f-86d8-3797ef1a91a0',
-                                                                             periodicity='b9f83ad5-7701-4098-bdaf-ee092f3247eb')
-        historical_selic_ad_data.delete()
+        historical_selic_daily_data = finance.models.FinanceData.objects.filter(index_id='b7e5c4a0-3b65-4b1f-86d8-3797ef1a91a0',
+                                                                                periodicity=DAILY_PERIODICITY)
+        historical_selic_daily_data.delete()
 
         list_selic_historical = []
-        for idx, item in selic_interest_ad.iterrows():
+        for idx, item in selic_interest_daily.iterrows():
             aux = finance.models.FinanceData(
                 name='SELIC',
                 index_id='b7e5c4a0-3b65-4b1f-86d8-3797ef1a91a0',
@@ -106,9 +110,9 @@ class BancoCentralAPI(Integration):
         ipca_am = pd.read_json(self.url_bcdata.format('433', ''))
         ipca_am['data'] = pd.to_datetime(ipca_am['data'], dayfirst=True)
 
-        historical_ipca_am_data = finance.models.FinanceData.objects.filter(index_id='ef07cbb0-9b29-43c6-a060-bef73f1cc000',
-                                                                            periodicity_id='dc5b3bf8-2b84-423a-9a90-e7e194e355fa')
-        historical_ipca_am_data.delete()
+        historical_ipca_monthly_data = finance.models.FinanceData.objects.filter(index_id='ef07cbb0-9b29-43c6-a060-bef73f1cc000',
+                                                                                 periodicity_id=MONTHLY_PERIODICITY)
+        historical_ipca_monthly_data.delete()
 
         list_ipca_historical = []
         for idx, item in ipca_am.iterrows():
@@ -129,7 +133,6 @@ class BancoCentralAPI(Integration):
         }
 
         return
-
 
     def __set_reference(self, date):
         year = date.year
